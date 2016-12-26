cc128cebf7c6c8616fb85d21205f6ed434230f98
BePsvPT <freedom@csie.io>
2016-12-26 14:44:01 +0800

    First commit

---
+++ src/SnappyServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Infoexam\Snappy;
+
+use Barryvdh\Snappy\Facades\SnappyImage;
+use Barryvdh\Snappy\Facades\SnappyPdf;
+use Illuminate\Support\ServiceProvider;
+
+class SnappyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->overrideConfig();
+    }
+
+    /**
+     * Override laravel snappy config.
+     *
+     * @return void
+     */
+    protected function overrideConfig()
+    {
+        if (PHP_OS !== 'Linux') {
+            return;
+        }
+
+        $bit = PHP_INT_SIZE === 4 ? 'i386' : 'amd64';
+
+        $path = base_path('vendor/h4cc/wkhtmlto%s-%s/bin/wkhtmlto%s-%s');
+
+        foreach (['pdf', 'image'] as $type) {
+            $this->app['config']->set(
+                sprintf('snappy.%s.binary', $type),
+                sprintf($path, $type, $bit, $type, $bit)
+            );
+        }
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(\Barryvdh\Snappy\ServiceProvider::class);
+
+        $this->app->alias('SnappyPdf', SnappyPdf::class);
+        $this->app->alias('SnappyImage', SnappyImage::class);
+    }
+}
---
+++ tests/SnappyTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class SnappyTest extends Orchestra\Testbench\TestCase
+{
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            Infoexam\Snappy\SnappyServiceProvider::class,
+        ];
+    }
+
+    public function test_service_provider_loaded()
+    {
+        $this->assertArrayHasKey(Barryvdh\Snappy\ServiceProvider::class, $this->app->getLoadedProviders());
+
+        $this->assertSame('SnappyImage', $this->app->getAlias(Barryvdh\Snappy\Facades\SnappyImage::class));
+        $this->assertSame('SnappyPdf', $this->app->getAlias(Barryvdh\Snappy\Facades\SnappyPdf::class));
+    }
+
+    /**
+     * @requires OS Linux
+     */
+    public function test_config()
+    {
+        $config = $this->app['config']['snappy'];
+
+        if (PHP_INT_SIZE === 4) {
+            $this->assertSame(base_path('vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386'), $config['pdf']['binary']);
+            $this->assertSame(base_path('vendor/h4cc/wkhtmltoimage-i386/bin/wkhtmltoimage-i386'), $config['image']['binary']);
+        } else {
+            $this->assertSame(base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'), $config['pdf']['binary']);
+            $this->assertSame(base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'), $config['image']['binary']);
+        }
+    }
+}
